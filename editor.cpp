30af8cd8605ded2dfeada6ffddee83fbcd834691
Fengtao Zhang <zhangfengtao1998@gmail.com>
2025-09-12 00:56:27 +0100

    chapter 3, Raw input and output "Reposition the cursor", "Clear the screen on exit", "tildes", "Global state", "Window size, the easy way" Done.

--- editor.cpp
+++ editor.cpp
@@ -2,6 +2,7 @@
 #include "ctype.h"
 #include "errno.h"
 #include "stdio.h"
+#include "sys/ioctl.h"
 #include "termios.h"
 #include "unistd.h"
 #include <cstdlib>
@@ -13,18 +14,25 @@
 
 /*** data ***/
 
-// struct containing all terminal information
-struct termios orig_termios;
+struct editorConfig {
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 /*** terminal ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
   perror(s);
   exit(1);
 }
 void disableRawMode() {
   // Dsiable Raw mode, use the original terminal info stored.
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
     die("tcsetattr");
   }
 }
@@ -32,13 +40,13 @@ void disableRawMode() {
 void enableRawModel() {
   // get the parameter associated with the terminal referred to by fildes and
   // store them in the termios structure referenced by termios_p
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) {
     die("tcgetattr");
   }
   // at exit of the program, call function disableRawMode()
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   // bit wise AND and assign with bitwise-NOT
   // Equavilent: raw.c_lflag = (raw.c_lflag) & (~(ECHO))
   raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
@@ -69,9 +77,35 @@ char editorReadKey() {
   return c;
 }
 
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 /*** output ***/
 
-void editorRefreshScreen() { write(STDOUT_FILENO, "\x1b[2J", 4); }
+void editorDrawRows() {
+  int y;
+  for (y = 0; y < E.screenrows; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editorDrawRows();
+
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
 
 /*** input ***/
 
@@ -80,6 +114,8 @@ void editorProcessKeypress() {
 
   switch (c) {
   case CTRL_KEY('q'):
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
     break;
   }
@@ -87,8 +123,15 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
+    die("getWindowSize");
+  }
+}
+
 int main() {
   enableRawModel();
+  initEditor();
 
   while (1) {
     editorRefreshScreen();
