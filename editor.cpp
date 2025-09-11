4f64b163a8a93dcf48cda3d7acb474267d7dd8c6
Fengtao Zhang <zhangfengtao1998@gmail.com>
2025-09-11 23:29:58 +0100

    chapter 3, Raw input and output "Clear the screen" done

--- editor.cpp
+++ editor.cpp
@@ -69,6 +69,10 @@ char editorReadKey() {
   return c;
 }
 
+/*** output ***/
+
+void editorRefreshScreen() { write(STDOUT_FILENO, "\x1b[2J", 4); }
+
 /*** input ***/
 
 void editorProcessKeypress() {
@@ -87,6 +91,7 @@ int main() {
   enableRawModel();
 
   while (1) {
+    editorRefreshScreen();
     editorProcessKeypress();
   }
   return 0;
