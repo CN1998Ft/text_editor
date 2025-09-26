577b59981aefb65a1ece7706dfddbf23a2f77a03
Fengtao Zhang <zhangfengtao1998@gmail.com>
2025-09-26 19:54:49 +0100

    license and disclaimer

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
+# Disclaimer
+
+> [!NOTE]
+> This project is a copy of Kilo, a small text editor created by antirez.
+> The original source code is available from GitHub [Kilo](https://github.com/antirez/kilo).
+>
+>Additionally, the awesome tutorial is available at [kilo](https://viewsourcecode.org/snaptoken/kilo/). Or under subfolder [kilo_document](./kilo_document/)
+
 # A simple text editor
 
 Started on: 2025-08-15 20:23
 
+The aim of this project is to understand how does a text editor function inside
+the terminal. Also, it is for me to practice with C/C++ as I am bad at it.
 
 ----
 ## LICENSE
--- editor.cpp
+++ editor.cpp
@@ -1,3 +1,65 @@
+/* Kilo -- A very simple editor in less than 1-kilo lines of code (as counted
+ *         by "cloc"). Does not depend on libcurses, directly emits VT100
+ *         escapes on the terminal.
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Original code is
+ * Copyright (C) 2016 Salvatore Sanfilippo <antirez at gmail dot com>
+ * and it is licensed under the BSD-2-Clause License (see below)
+ *
+ * This derivative work is
+ * Copyright (c) 2025 Fengtao Zhang
+ *
+ * All rights reserved.
+ *
+ * --- BEGIN BSD-2-Clause LICENSE TEXT ---
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *  *  Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *  *  Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ---END BSD-2-Clause LICENSE TEXT ---
+
+ * ---BEGIN MIT LICENSE TEXT ---
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * ---END MIT LICENSE TEXT ---
+ */
+
 /*** inlcudes ***/
 #include "ctype.h"
 #include "errno.h"
