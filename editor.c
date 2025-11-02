971fdd4351fc37443d6b59cfe91fd5f3a17cac67
ft_linux <zhangfengtao1998@gmail.com>
2025-11-02 22:40:32 +0000

    Chapter 4   Read and display the first line of file now.

--- editor.c
+++ editor.c
@@ -61,6 +61,10 @@
  */
 
 /*** inlcudes ***/
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -267,15 +271,29 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** file i/o ***/
 
-void editorOpen() {
-  char *line = "Hello, world!";
-  ssize_t linelen = 13;
+void editorOpen(char *filename) {
+  FILE *fp = fopen(filename, "r");
+  if (!fp) {
+    die("fopen");
+  }
 
-  E.row.size = linelen;
-  E.row.chars = malloc(linelen + 1);
-  memcpy(E.row.chars, line, linelen);
-  E.row.chars[linelen] = '\0';
-  E.numrows = 1;
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  linelen = getline(&line, &linecap, fp);
+  if (linelen != -1) {
+    while (linelen > 0 &&
+           (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) {
+      linelen--;
+    }
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen + 1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;
+  }
+  free(line);
+  fclose(fp);
 }
 
 /*** append buffer ***/
@@ -306,7 +324,7 @@ void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
     if (y >= E.numrows) {
-      if (y == E.screenrows / 3) {
+      if (E.numrows == 0 && y == E.screenrows / 3) {
         char welcome[80];
         int welcomelen = snprintf(welcome, sizeof(welcome),
                                   "text editor -- version  %s", KILO_VERSION);
@@ -322,7 +340,7 @@ void editorDrawRows(struct abuf *ab) {
           abAppend(ab, " ", 1);
         }
         abAppend(ab, welcome, welcomelen);
-      } else if (y == E.screenrows / 3 + 1) {
+      } else if (E.numrows == 0 && y == E.screenrows / 3 + 1) {
         char welcome_1[80];
         int welcomelen_1 = snprintf(welcome_1, sizeof(welcome_1), "By Fengtao");
         if (welcomelen_1 > E.screencols) {
@@ -442,10 +460,12 @@ void initEditor() {
   }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
   enableRawModel();
   initEditor();
-  editorOpen();
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
 
   while (1) {
     editorRefreshScreen();
