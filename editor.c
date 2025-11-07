35ab39586a229577f789cf335542f83684518368
ft_linux <zhangfengtao1998@gmail.com>
2025-11-07 23:02:00 +0000

    Move left at the start of line

--- editor.c
+++ editor.c
@@ -434,6 +434,9 @@ void editorMoveCursor(int key) {
   case LEFT:
     if (E.cx != 0) {
       E.cx--;
+    } else if (E.cy > 0) {
+      E.cy--;
+      E.cx = E.row[E.cy].size;
     }
     break;
   case RIGHT:
