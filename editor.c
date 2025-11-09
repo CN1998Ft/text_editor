f40a27c01c2f8e5106ce7cdd36fcf054241e9c16
ft_linux <zhangfengtao1998@gmail.com>
2025-11-09 22:52:13 +0000

    search started

--- editor.c
+++ editor.c
@@ -297,11 +297,27 @@ int editorRowCxToRx(erow *row, int cx) {
   int j;
   for (j = 0; j < cx; j++) {
     if (row->chars[j] == '\t') {
-      rx += (KILO_TAB_STOP - 1) - (rx & KILO_TAB_STOP);
+      rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
     }
     rx++;
   }
   return rx;
+}
+
+int editorRowRxToCx(erow *row, int rx) {
+  int cur_rx = 0;
+  int cx;
+  for (cx = 0; cx < row->size; cx++) {
+    if (row->chars[cx] == '\t') {
+      cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+    }
+    cur_rx++;
+
+    if (cur_rx > rx) {
+      return cx;
+    }
+  }
+  return cx;
 }
 
 void editorUpdateRow(erow *row) {
@@ -518,6 +534,29 @@ void editorSave() {
   free(buf);
   editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
+
+/*** find ***/
+
+void editorFind() {
+  char *query = editorPrompt("Search: %s (ESC to cancel)");
+  if (query == NULL) {
+    return;
+  }
+
+  int i;
+  for (i = 0; i < E.numrows; i++) {
+    erow *row = &E.row[i];
+    char *match = strstr(row->render, query);
+    if (match) {
+      E.cy = i;
+      E.cx = editorRowRxToCx(row, match - row->render);
+      E.rowoff = E.numrows;
+      break;
+    }
+  }
+  free(query);
+}
+
 /*** append buffer ***/
 
 struct abuf {
@@ -788,6 +827,9 @@ void editorProcessKeypress() {
     if (E.cy < E.numrows) {
       E.cx = E.row[E.cy].size;
     }
+  case CTRL_KEY('f'):
+    editorFind();
+    break;
 
   case BACKSPACE:
   case CTRL_KEY('h'):
@@ -854,7 +896,7 @@ int main(int argc, char *argv[]) {
     editorOpen(argv[1]);
   }
 
-  editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
+  editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
   while (1) {
     editorRefreshScreen();
     editorProcessKeypress();
