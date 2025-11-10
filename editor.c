81998e9450e0f551914bdb908d08996654be5619
ft_linux <zhangfengtao1998@gmail.com>
2025-11-10 23:15:57 +0000

    restore color after search

--- editor.c
+++ editor.c
@@ -577,9 +577,18 @@ void editorFindCallback(char *query, int key) {
   static int last_match = -1;
   static int direction = 1;
 
+  static int saved_hl_line;
+  static char *saved_hl = NULL;
+
+  if (saved_hl) {
+    memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+    free(saved_hl);
+    saved_hl = NULL;
+  }
   if (key == '\r' || key == '\x1b') {
     last_match = -1;
     direction = 1;
+    return;
   } else if (key == RIGHT || key == DOWN) {
     direction = 1;
   } else if (key == LEFT || key == UP) {
@@ -610,6 +619,9 @@ void editorFindCallback(char *query, int key) {
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
 
+      saved_hl_line = current;
+      saved_hl = malloc(row->size);
+      memcpy(saved_hl, row->hl, row->rsize);
       memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
       break;
     }
